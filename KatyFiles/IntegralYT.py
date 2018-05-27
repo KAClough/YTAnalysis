bb1932d9d2bcc8a434022b9b61f51549cf74b50c
KAClough <katyclough@yahoo.com>
2018-05-27 15:29:42 +0200

    Useful amends

--- KatyFiles/IntegralYT.py
+++ KatyFiles/IntegralYT.py
@@ -9,25 +9,35 @@ def _HamSquared(field, data) :
     value = data["Ham"] * data["Ham"]
     return value
 
-loc = '/scratch2/kclough/Inflation/Inf9HROldchk0*'
+@derived_field(name="RhoChi", units="")
+def _RhoChi(field, data) :
+    value = data["rho"] * ( data["chi"]**(-1.5) )
+    return value
+filename = 'RhoChiOut.dat'
+loc = '/scratch/shared/pr1ukclo/ASBH/r120M1a0phi0.08HRplt00*'
 ds = yt.load(loc)
 
-time_data = []
-integral_data = []
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+		
+#integral_data = []
+
+volume = ( float(ds[0].domain_right_edge[0]) )**3.0
 
 for i in ds :
-    field = 'HamSquared'
+    #field = 'HamSquared'
+    field = 'RhoChi'
     weight = 'cell_volume'
     ad = i.all_data()
     #ad2 = ad.cut_region('obj["chi"] > 0.1')
 
     integral = ad.quantities.weighted_average_quantity(field, weight)
-    #integral = ad2.quantities.total_quantity(field)
+    #integral = ad.quantities.total_quantity(field)
+    integral = integral * volume
 
-    integral = np.sqrt(integral)
+    if(comm.rank==0) :
+        datafile=open(filename,'ab')
+        datafile.write("%f    %f \n" % (i.current_time, integral))
+        datafile.close()
 
-    time_data.append(i.current_time)
-    integral_data.append(integral)
-
-np.savetxt('HamTime_old.out', time_data)
-np.savetxt('AvHam_old.out', integral_data)
+#np.savetxt('RhoChi.out', integral_data)
